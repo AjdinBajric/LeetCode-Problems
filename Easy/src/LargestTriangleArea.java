4de7f2560b2a5e8ce1b72c9ba8b5ae6af9ccc395
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-07-21 18:19:17 +0200

    Added *Largest triangle area*

---
+++ Easy/src/LargestTriangleArea.java
@@ -0,0 +1,26 @@
+
+public class LargestTriangleArea {
+
+	public static double largestTriangleArea(int[][] points) {
+        int N = points.length;
+        double ans = 0;
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j)
+                for (int k = j+1; k < N; ++k)
+                    ans = Math.max(ans, area(points[i], points[j], points[k]));
+        return ans;
+    }
+
+    public static double area(int[] P, int[] Q, int[] R) {
+        return 0.5 * Math.abs(P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1]
+                             -P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]);
+    }
+	
+	public static void main(String[] args) {
+		
+		// First test case
+		int [][] points ={{0,0},{0,1},{1,0},{0,2},{2,0}};
+		System.out.println(largestTriangleArea(points));
+	}
+
+}
