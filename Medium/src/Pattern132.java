3b671d56fcdec450fe7a99f2ea1b1c140949850f
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-07-24 21:07:01 +0200

    Update Pattern132.java
    Corrected method "find132pattern"

--- Medium/src/Pattern132.java
+++ Medium/src/Pattern132.java
@@ -2,16 +2,31 @@
 public class Pattern132 {
 
     public static boolean find132pattern(int[] nums) {
-        
-    	int first=0, second = 0, third = 0, first_n=0, second_n=0, third_n=0;
-    	for(int i=0;i<nums.length;i++){
-    		if(nums[i]==1){first=i; first_n=nums[i];}
-    		if(nums[i]==3){ second=i; second_n=nums[i];}
-    		if(nums[i]==2){ third=i;third_n=nums[i];}
-    	}
-    	
-    	if(first<second && second<third && first_n<second_n && second_n<third_n ){return true;}
-    	else return false;
+	    
+        if (nums.length < 3)
+            return false;
+	    
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+	    
+        for (int i = 1; i < nums.length; i++)
+		
+            min[i] = Math.min(min[i - 1], nums[i]);
+	    
+        for (int j = nums.length - 1, k = nums.length; j >= 0; j--) {
+		
+            if (nums[j] > min[j]) {
+		    
+                while (k < nums.length && nums[k] <= min[j])
+                    k++;
+		    
+                if (k < nums.length && nums[k] < nums[j])
+                    return true;
+		    
+                nums[--k] = nums[j];
+            }
+        }
+        return false;
     }
 	
 	public static void main(String[] args) {
