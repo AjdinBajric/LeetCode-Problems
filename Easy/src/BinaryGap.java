7d80a8640dac7f45a3584c36406f20afac001ad7
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-07-26 22:17:30 +0200

    Added *Binary Gap*

---
+++ Easy/src/BinaryGap.java
@@ -0,0 +1,23 @@
+
+public class BinaryGap {
+
+    public static int binaryGap(int N) {
+        int[] A = new int[32];
+        int t = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((N >> i) & 1) != 0)
+                A[t++] = i;
+
+        int ans = 0;
+        for (int i = 0; i < t - 1; ++i)
+            ans = Math.max(ans, A[i+1] - A[i]);
+        return ans;
+    }
+	
+	public static void main(String[] args) {
+		
+		System.out.println(binaryGap(44));
+
+	}
+
+}
