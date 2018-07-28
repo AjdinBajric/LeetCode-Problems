39b0fbef8f20820a66914c5abb641b1676d6a380
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-07-28 22:22:36 +0200

    Added *Binary Number with Alternating Bits*

---
+++ Easy/src/BinaryNumberwithAlternatingBits.java
@@ -0,0 +1,21 @@
+public class BinaryNumberwithAlternatingBits {
+
+    public static boolean hasAlternatingBits(int n) {
+        /*
+        n =         1 0 1 0 1 0 1 0
+        n >> 1      0 1 0 1 0 1 0 1
+        n ^ n>>1    1 1 1 1 1 1 1 1
+        n           1 1 1 1 1 1 1 1
+        n + 1     1 0 0 0 0 0 0 0 0
+        n & (n+1)   0 0 0 0 0 0 0 0
+        */
+
+        n = n ^ (n>>1);
+        return (n & n+1) == 0;
+    }
+	public static void main(String[] args) {
+
+		System.out.println(hasAlternatingBits(5));
+	}
+
+}
