a26131b1c3952152f2e812a82667a2be945fd61f
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-08-02 19:42:25 +0200

    Added * Excel Sheet Column Number*

---
+++ Easy/src/ExcelSheetColumnNumber.java
@@ -0,0 +1,20 @@
+public class ExcelSheetColumnNumber {
+
+	public static int titleToNumber(String s) {
+
+		int res = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			res = res * 26 + (ch - 'A' + 1);
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(titleToNumber("Ajdin"));
+
+	}
+
+}
