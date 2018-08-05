6f4305c21aa256bc00101e46051e7cbf7af3169c
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-08-05 22:08:26 +0200

    Added *Number of Segments in a String*

---
+++ Easy/src/NumberOfSegmentsInAString.java
@@ -0,0 +1,22 @@
+public class NumberOfSegmentsInAString {
+
+	public static int countSegments(String s) {
+
+		int count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(countSegments(" Hello, my name is Ajdin  "));
+
+	}
+
+}
