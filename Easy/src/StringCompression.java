fac5343f6c9ea6449bf78b0025ecaeeeb8758902
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-08-11 23:18:11 +0200

    Added *String Compression*

---
+++ Easy/src/StringCompression.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCompression {
+
+	public static int compress(char[] chars) {
+		int indexAns = 0, index = 0;
+		while (index < chars.length) {
+			char currentChar = chars[index];
+			int count = 0;
+			while (index < chars.length && chars[index] == currentChar) {
+				index++;
+				count++;
+			}
+			chars[indexAns++] = currentChar;
+			if (count != 1)
+				for (char c : Integer.toString(count).toCharArray())
+					chars[indexAns++] = c;
+		}
+		return indexAns;
+	}
+
+	public static void main(String[] args) {
+
+		char[] array = { 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b' };
+		System.out.println(compress(array));
+
+	}
+
+}
